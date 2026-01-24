35098013d16c12ba927aceba836d42727b99f0da
Atharv Ingale <atharv.i@humro.com>
2026-01-24 19:06:41 +0530

    config chnages

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base:"/PORT-FOLIO",
+  base:"/PORT-FOLIO/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
